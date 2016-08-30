e38433dd92f2706505add2a72db42f2236e57f9f
Yannick <yannick.darcillon@gmail.com>
2016-08-30 14:41:51 +0200

    rename

---
+++ BowlingTestProject/BowlingUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using Bowling;
+
+namespace BowlingTestProject
+{
+    [TestFixture]
+    public class BowlingUnitTest
+    {
+        [Test]
+        public void TestBasicScoring(  )
+        {
+            Game myGame = new Game("11111111111111111111");
+            Assert.AreEqual(myGame.total(), 20 );
+        }
+
+
+        [Test]
+        public void TestAllStrikeScoring()
+        {
+            Game myGame = new Game("XXXXXXXXXXXX");
+            Assert.AreEqual(myGame.total(), 300);
+        }
+
+        [Test]
+        public void TestAllStrikeThenMissScoring()
+        {
+            Game myGame = new Game("XXXXXXXXXX--");
+            Assert.AreEqual(myGame.total(), 270);
+        }
+
+        [Test]
+        public void TestAllSpare()
+        {
+            Game myGame = new Game("1/1/1/1/1/1/1/1/1/1/1");
+            Assert.AreEqual(myGame.total(), 110);
+        }
+    }
+}
+
